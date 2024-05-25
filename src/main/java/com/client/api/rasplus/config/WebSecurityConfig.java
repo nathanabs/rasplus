71c4f35a8c04284896252fa05429b555d1ebedc0
Nathan Aguiar Barbosa Silva <nathanaguiarbs@gmail.com>
2024-05-25 15:32:27 -0300

    Spring security

---
+++ src/main/java/com/client/api/rasplus/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.client.api.rasplus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.GET, "/subscription-type/**")
+                        .permitAll().anyRequest().authenticated())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS));
+        return http.build();
+    }
+
+}
--- src/main/java/com/client/api/rasplus/mapper/UserPaymentInfoMapper.java
+++ src/main/java/com/client/api/rasplus/mapper/UserPaymentInfoMapper.java
@@ -16,6 +16,7 @@ public class UserPaymentInfoMapper {
                 .price(dto.getPrice())
                 .datePayment(dto.getDatePayment())
                 .user(user)
+                .installments(dto.getInstalments())
                 .build();
     }
 }
